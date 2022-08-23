d150b1038504f4900d7071cefd9c62785bb15d33
cceliuss187 <cceliuss187@gmail.com>
2022-08-22 20:09:47 -0400

    Edited the script

--- process.sh
+++ process.sh
@@ -2,5 +2,8 @@
 
 #this script prints out the memory and CPU usage from my Wsl and outputs the results to the processes.txt file
 
-ps -aux | awk '{print $3,$4}' |sudo tee processes.txt
+curnt_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+
+ps -aux | awk '{print $3,$4}' && echo "the current time is : $curnt_time" |sudo tee processes.txt
 
